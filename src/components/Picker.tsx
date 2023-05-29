c9c83b6b606aa697787bfb027cbadaae6c6485a4
David Reed <reedswork@proton.me>
2023-05-29 18:55:08 -0400

    refactor picker component
    removed unused types
    made it more functional

--- src/components/Picker.tsx
+++ src/components/Picker.tsx
@@ -1,42 +1,66 @@
-import type { MouseEventHandler } from "react";
-import React, { useRef } from "react";
+import type { ChangeEventHandler, MouseEventHandler } from "react";
+import React, { forwardRef, useRef } from "react";
 import Control from "./Control.js";
 import type { BaseControlProps } from "./Control.js";
 
 export interface PickerProps extends BaseControlProps {
-  accept?: string;
-  multiple?: boolean;
+  /**
+   * Value of the path input. Use defaultValue if the path input should be editable
+   */
   value?: string;
-  onBrowse: MouseEventHandler<HTMLInputElement>;
+  /**
+   * Default value of the path input
+   */
+  defaultValue?: string;
+  /**
+   * If the path input should be editable or read only.
+   */
+  readOnly?: boolean;
+  /**
+   * Called when the path input is updated
+   */
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  /**
+   * Called when the browse button is pressed
+   */
+  onBrowse?: MouseEventHandler<HTMLInputElement>;
 }
 
 /**
  * Generic path picker
  * Compatible with native clients and browsers
  */
-export default function Picker({
-  title,
-  attention,
-  description,
-  value,
-  onBrowse,
-}: PickerProps) {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
+const Picker = forwardRef<HTMLInputElement, PickerProps>(function Picker(
+  {
+    title,
+    attention,
+    description,
+    value,
+    defaultValue,
+    readOnly,
+    onChange,
+    onBrowse,
+  },
+  ref
+) {
   return (
     <Control title={title} attention={attention} description={description}>
       <div className="settingsBtn" style={{ width: 100 }} onClick={onBrowse}>
         Browse
       </div>
       <input
-        ref={inputRef}
+        ref={ref}
         type="text"
         name="text"
         className="inputGrey2"
         value={value}
+        defaultValue={defaultValue}
         spellCheck={false}
-        readOnly
+        readOnly={readOnly}
+        onChange={onChange}
       />
     </Control>
   );
-}
+});
+
+export default Picker;
